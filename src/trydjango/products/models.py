a20ae19538ee63563e88c483bb27a3a054b3b022
Rafael Rêgo <rafael.rego@loggi.com>
2019-06-02 18:44:16 -0300

    Add featured boolean to products model

--- src/trydjango/products/models.py
+++ src/trydjango/products/models.py
@@ -7,3 +7,4 @@ class Product(models.Model):
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(decimal_places=2, max_digits=10)
     summary = models.TextField()
+    featured = models.BooleanField()
