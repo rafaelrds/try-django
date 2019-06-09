b8f1c0b39361d5af7b70d5b1b1cd33e27c14f6dd
Rafael Rêgo <rafael.rego@loggi.com>
2019-06-09 20:03:12 -0300

    Add more views

--- src/trydjango/pages/views.py
+++ src/trydjango/pages/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 
 
 # Create your views here.
-def home_view(*args, **kwargs):
+def home_view(request, *args, **kwargs):
     return HttpResponse("<h1>Hello World</h1>")
+
+
+def contact_view(request, *args, **kwargs):
+    print(args, kwargs)
+    return HttpResponse("<h1>Contact View</h1>")
