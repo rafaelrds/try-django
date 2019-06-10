b89836ecc63155dd3f2607064c11b87de128516b
Rafael Rêgo <rafael.rego@loggi.com>
2019-06-09 23:45:12 -0300

    Add templates for home, about and contact

--- src/trydjango/pages/views.py
+++ src/trydjango/pages/views.py
@@ -1,11 +1,15 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html", {})
 
 
 def contact_view(request, *args, **kwargs):
-    print(args, kwargs)
-    return HttpResponse("<h1>Contact View</h1>")
+    return render(request, "contact.html", {})
+
+
+def about_view(request, *args, **kwargs):
+    return render(request, "about.html", {})
